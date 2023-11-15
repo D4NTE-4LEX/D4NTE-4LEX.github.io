6c6542d2d923b096a1e868fba0f60ceb0bb03302
IDIC <alumnos@alumnosuio.ute.edu.ec>
2023-11-15 15:35:47 -0500

    fix(script): Validations were created

--- js/script.js
+++ js/script.js
@@ -22,8 +22,19 @@ function multiplica(mivariable, suma){
 
 function resEqSecGrade(a, b, c){
     let x1, x2;
+    if(a == 0){
+        window.alert("El coeficiente cuadratico no puede ser cero");
+        return;
+    }
+    if(Math.pow(b, 2) - 4 * a * c < 0){
+        window.alert("No existen soluciones reales");
+        return;
+    }
+    if(Math.pow(b, 2) - 4 * a * c == 0){
+        window.alert("Existen soluciones reales e iguales");
+    }
     x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
     x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
-    window.alert("x1 = " + x1 + " x2 = " + x2);
+    window.alert(a+"x^2 + "+b+"x + "+c+"\nx1 = " + x1 + " x2 = " + x2);
 }
 //multiplica(mivariable, suma);
