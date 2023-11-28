a920edc28ee236e140fd1b9a20272ed329966f59
D4NTE-4LEX <dannyjacome78gmail.com>
2023-11-27 21:55:16 -0500

    feat(JQuery): Exercises completed

--- js/jquery.js
+++ js/jquery.js
@@ -52,6 +52,57 @@ $(document).ready(() => {
         $('#panelEx7').stop();
     });
 
+    let ex8Button = $('#ex8Button');
+    ex8Button.click(() => {
+        alert('Value: ' + $('#ex8').val());
+    });
+
+    let btn1ex9 = $('#btn1Ex9');
+    let btn2ex9 = $('#btn2Ex9');
+    let btn3ex9 = $('#btn3Ex9');
+    btn1ex9.click(() => {
+        $('#test1Ex9').text('Hello world!');
+    });
+    btn2ex9.click(() => {
+        $('#test2Ex9').html('<b>Hello world!</b>');
+    });
+    btn3ex9.click(() => {
+        $('#test3Ex9').val('Dolly Duck');
+    });
+
+    let btn1Ex10 = $('#btn1Ex10');
+    let btn2Ex10 = $('#btn2Ex10');
+    btn1Ex10.click(() => {
+        $('[id=ex10]').append(' <b>Appended text</b>.');
+    });
+    btn2Ex10.click(() => {
+        $('#olEx10').append('<li>Appended item</li>');
+    });
+
+    let btnEx11 = $('#btnEx11');
+    btnEx11.click(() => {
+        $('#ex11').remove();
+    });
+
+    let btnEx12 = $('#btnEx12');
+    btnEx12.click(() => {
+        $('#ex12').addClass('blue');
+        $('#divEx12').addClass('important');
+    });
+
+    let btnEx13 = $('#btnEx13');
+    btnEx13.click(() => {
+        alert("Background color = " + $('#ex13').css('background-color'));
+    });
+
+    let btnEx14 = $('#btnEx14');
+    btnEx14.click(() => {
+        var txt = "";
+        txt += "Width of div: " + $('#divEx14').width() + "</br>";
+        txt += "Height of div: " + $('#divEx14').height();
+        $('#divEx14').html(txt);
+    });
+
     let addTask = (text) => {
         let newTask = $('<li><p>' + text + '<p></li>');
         newTask.click(function() {
